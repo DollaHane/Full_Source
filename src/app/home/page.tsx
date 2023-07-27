1eb1282fa29736eb4f90d0f27cc7d5e6182b649e
DollaHane <shanehubsch@gmail.com>
2023-07-27 15:34:18 +0200

    updated navbar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,38 @@
 import React from "react"
+import Link from "next/link"
+import { MainNav } from "@/src/components/MainNav"
+import { UserAccountNav } from "@/src/components/UserAccountNav"
+import { ThemeToggle } from "@/src/components/components-global/theme-toggle"
+import { buttonVariants } from "@/src/components/components-ui/Button"
 import GeneralFeed from "@/src/components/pageHome/GeneralFeed"
+import { siteConfig } from "@/src/config/site"
+import { authOptions } from "@/src/lib/auth"
+import { getServerSession } from "next-auth"
 
-import { NavBar } from "../../components/NavBar"
-
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
   return (
     <div className="h-auto w-full">
-      {/* NAVBAR */}
-      <NavBar />
+      <header className="sticky top-0 z-40 w-full border-b bg-background">
+        <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+          <MainNav items={siteConfig.mainNav} />
+          <div className="flex flex-1 items-center justify-end space-x-4">
+            <nav className="flex items-center space-x-5">
+              {/* SIGN IN */}
+              {session?.user ? (
+                <UserAccountNav user={session.user} />
+              ) : (
+                <Link href="/sign-in" className={buttonVariants()}>
+                  Sign In
+                </Link>
+              )}
+
+              {/* THEME BUTTON */}
+              <ThemeToggle />
+            </nav>
+          </div>
+        </div>
+      </header>
 
       {/* FEED */}
       <GeneralFeed />
