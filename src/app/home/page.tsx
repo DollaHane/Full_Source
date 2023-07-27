e295f679d611f22d56361e8d9a8cb0e47a688e3d
DollaHane <shanehubsch@gmail.com>
2023-07-27 23:01:15 +0200

    Updated navbar with admin props

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,46 +1,17 @@
 import React from "react"
-import Link from "next/link"
-import { db } from "@/src/lib/db"
-import { MainNav } from "@/src/components/MainNav"
-import { UserAccountNav } from "@/src/components/UserAccountNav"
-import { ThemeToggle } from "@/src/components/components-global/theme-toggle"
-import { buttonVariants } from "@/src/components/components-ui/Button"
+import { NavBar } from "@/src/components/NavBar"
 import GeneralFeed from "@/src/components/pageHome/GeneralFeed"
-import { siteConfig } from "@/src/config/site"
 import { authOptions } from "@/src/lib/auth"
 import { getServerSession } from "next-auth"
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
 
-  const user = await db.user.findUnique({
-    where: {
-      email: session?.user.email as string | undefined,
-    },
-  })
-
   return (
     <div className="h-auto w-full">
-      <header className="sticky top-0 z-40 w-full border-b bg-background">
-        <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-          <MainNav items={siteConfig.mainNav} />
-          <div className="flex flex-1 items-center justify-end space-x-4">
-            <nav className="flex items-center space-x-5">
-              {/* SIGN IN */}
-              {session?.user && user ? (
-                <UserAccountNav user={session.user} admin={user.admin}/>
-              ) : (
-                <Link href="/sign-in" className={buttonVariants()}>
-                  Sign In
-                </Link>
-              )}
-
-              {/* THEME BUTTON */}
-              <ThemeToggle />
-            </nav>
-          </div>
-        </div>
-      </header>
+      {/* NAVBAR */}
+       {/* @ts-expect-error Server Component */}
+      <NavBar/>
 
       {/* FEED */}
       {/* @ts-expect-error Server Component */}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,19 @@ import { ThemeToggle } from "@/src/components/components-global/theme-toggle"
 import { siteConfig } from "@/src/config/site"
 import { authOptions } from "@/src/lib/auth"
 import { getServerSession } from "next-auth"
-
+import { db } from "../lib/db"
 import { MainNav } from "./MainNav"
 import { UserAccountNav } from "./UserAccountNav"
 import { buttonVariants } from "./components-ui/Button"
 
 export async function NavBar() {
   const session = await getServerSession(authOptions)
+
+  const user = await db.user.findUnique({
+    where: {
+      email: session?.user.email as string | undefined,
+    },
+  })
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
@@ -18,8 +24,8 @@ export async function NavBar() {
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-5">
             {/* SIGN IN */}
-            {session?.user ? (
-              <UserAccountNav user={session.user} />
+            {session?.user && user ? (
+              <UserAccountNav user={session.user} admin={user.admin}/>
             ) : (
               <Link href="/sign-in" className={buttonVariants()}>
                 Sign In
