3b27c56df90a32b8cb272616934625e8eb02df2d
DollaHane <shanehubsch@gmail.com>
2023-08-12 00:01:02 +0200

    image test

--- src/components/renderers/CustomImageRenderer.tsx
+++ src/components/renderers/CustomImageRenderer.tsx
@@ -6,7 +6,7 @@ function CustomImageRenderer({ data }: any) {
   const src = data.file.url
 
   return (
-    <div className="relative my-5 min-h-[25rem] w-full">
+    <div className="relative my-5 min-h-[25rem] w-9/12">
       <Image
         alt="image"
         className="object-contain"
