a166039985d70f6fbad7783d7f49b1f66e8fcdf4
DollaHane <shanehubsch@gmail.com>
2023-08-27 14:45:50 +0200

    Major UI Updates

--- src/app/fs/home/page.tsx
+++ src/app/fs/home/page.tsx
@@ -1,8 +1,4 @@
 import React from "react"
-import LeftNav from "@/src/components/LeftNav"
-import { NavBar } from "@/src/components/NavBar"
-import { SideNav } from "@/src/components/SideNav"
-import NpmFeed from "@/src/components/pageHome/NpmFeed"
 import PostFeed from "@/src/components/pageHome/PostFeed"
 import { db } from "@/src/lib/db"
 
