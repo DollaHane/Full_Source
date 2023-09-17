96b7225d481a77b6454b0fb1b6f6c3fb283a9aa1
DollaHane <shanehubsch@gmail.com>
2023-09-17 13:45:56 +0200

    updated image link with _blank

--- src/components/renderers/CustomImageRenderer.tsx
+++ src/components/renderers/CustomImageRenderer.tsx
@@ -7,7 +7,7 @@ function CustomImageRenderer({ data }: any) {
 
   return (
     <div className="relative mb-12 mt-5 w-full md:w-1/2">
-      <Link href={src}>
+      <Link href={src} target="_blank">
         <img
           alt="image"
           className="h-auto max-h-96 rounded-lg object-contain shadow-lg"
